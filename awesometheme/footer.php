7a8f6c2215f13a0055b1c240cc470c8a8cc4ebe3
RIchard Santana <121190316+Drahxa@users.noreply.github.com>
2024-09-25 12:09:34 -0400

    Add files via upload

---
+++ awesometheme/footer.php
@@ -0,0 +1,11 @@
+    
+    <footer>
+        <p> This is the Footer</p>
+        <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
+    </footer>
+
+    <?php wp_footer(); ?>
+
+    </body>
+    
+</html>
---
+++ awesometheme/page-6.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<?php if ( have_posts() ):
+    
+    while( have_posts()): the_post();?>
+    
+    <p>I Am A Sigma </p>
+        <p><?php the_content();?></p>
+
+        <h3><?php the_title();?></h3>
+         
+        <hr>
+    <?php endwhile;
+
+endif;
+
+    ?>
+
+<?php get_footer(); ?> 
